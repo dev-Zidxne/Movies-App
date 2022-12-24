941698886c1d2cf549212690d54679af4505171c
Zidane Innis <zidaneinnis@gmail.com>
2022-12-24 09:39:50 -0500

    Sending a GET request pt 1

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  function fetchMoviesHandler() {
+    fetch("https://swapi.dev/api/films")
+      .then((response) => {
+        return response.json();
+      })
+      .then();
+  }
+  return (
+    <React.Fragment>
+      <section>
+        <button>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={dummyMovies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
